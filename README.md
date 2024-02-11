45590c3ed9235a9294e78f9fa6dd7a312c36db48
Graciela Torres <grace4torres@gmail.com>
2024-02-11 10:03:28 -0600

    Modifique archivo.write

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ProyectoEjemplo
 Repositorio de Ejemplos 
+Aplicar la metodología CRUD. Create, Read Update and Read 
--- proy_01.py
+++ proy_01.py
@@ -4,19 +4,37 @@ DIRECTORIO = "dir/"
 EXTENSION = ".txt"
 
 class Platillo:
-    def __init__(self,nombre,ingredientes, termino, costo):
+    def __init__(self,nombre,ingredientes, termino, categoria):
         self.nombre = nombre
         self.ingredientes = ingredientes
         self.termino = termino
-        self.costo = costo
+        self.categoria = categoria
+        
 def crear_platillo():
     nombre_platillo = input("Escribe el nombre del platillo:")
-    existe = existe_contacto(nombre_platillo)
+    existe = existe_platillo(nombre_platillo)
     #crear el archivo con el nombre del archivo
     if not existe:
         with open(DIRECTORIO + nombre_platillo + EXTENSION, "w") as archivo:
-            archivo.write()
+            archivo.write()#Aqui no le estoy mandado nada para que escriba, por lo que hay que llamar a la clase con sus atributos
+            #Escribir los otros campos en el archivo creado
+            ingredientes_platillo = input("Escribe los ingredientes del platillo: \r\n")
+            termino_platillo = input("Escribe el termino del platillo: \r\n")
+            categoria_platillo = input("Escribe la categoria del platillo: \r\n")
+            #Instanciar el construcor de la clase 
+            platillo = Platillo(nombre_platillo, ingredientes_platillo, termino_platillo, categoria_platillo)
+
+            #Todavia no escribe porque no le hemos mandado los  datos al método para guardalos
+            #referencia a la Class Contacto NombredelObjeto.Nombredelatributo
+            archivo.write(platillo.ingredientes)
+            archivo.write(platillo.termino)
+            archivo.write(platillo.categoria)
+            #Mostrar mensaje  
+            print("\r\nContacto Agregado Exitosamente\r\n")               
     else:
+        print("El contacto ya existe.")
+
+    
         print("El platilla ya esta creado")
 
 
@@ -56,7 +74,7 @@ def crear_carpeta():
     except OSError as error:
         print(f"La carpeta llamada {DIRECTORIO} fue creada exitosamente\r\n")
 
-def existe_contacto(nombre):
+def existe_platillo(nombre):
    return os.path.isfile(DIRECTORIO + nombre + EXTENSION)
 
 def app():
