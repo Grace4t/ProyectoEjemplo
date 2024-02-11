87bb105b827e123605aa401ed1be60d6afe393a8
Graciela Torres <grace4torres@gmail.com>
2024-02-10 20:42:16 -0600

    Agregue función MostrarMen()

--- proy_01.py
+++ proy_01.py
@@ -3,14 +3,24 @@ import os
 DIRECTORIO = "dir/"
 EXTENCION = ".txt"
 
+def mostrar_menu():
+    print("SELECCIONE UNA OPCIÓN DEL MENÚ")
+    print("1. Crear platillo")
+    print("2. Editar platillo")
+    print("3. Mostrar platillo")
+    print("4. Buscar platillo")
+    print("5. Eliminar platillo")
+    
+
 def crear_carpeta():
     try:
         os.path.exists(DIRECTORIO)
         os.makedirs(DIRECTORIO)
     except OSError as error:
-        print("carpeta creada")
+        print(f"La carpeta llamada {DIRECTORIO} fue creada exitosamente")
 
 def app():
     crear_carpeta()
+    mostrar_menu()
 
 app()
