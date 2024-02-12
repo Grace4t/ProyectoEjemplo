e84fb8c83fb9a138fbc75c2dd4027ae197b1a15a
Graciela Torres <grace4torres@gmail.com>
2024-02-12 13:40:37 -0600

    Agregue funcion crear carpets

--- proy_02.py
+++ proy_02.py
@@ -1,9 +1,20 @@
+import os
+
 class Carpetas:
     def __init__(self,nombre):
         self.nombre = nombre
+
 def crear_carpeta():
-    print("Crear carpeta")
+    #necesito directorio
+    #crear un directorio con with y despues checar si ya existe ese direcionro
+    carpeta = input("ingresa el nombre de la carpeta: ")
+    car_prin = carpeta+"/"
+    print(car_prin)
+    if not os.path.exists(car_prin):
+        os.makedirs(car_prin)
+
+    
 def app():
-    print("App principal!")
+    #print("App principal!")
     crear_carpeta()
 app()
